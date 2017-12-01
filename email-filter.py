3e3ba9869a2b6efa8e509d38d5100df87d9fd2a3
lamani hani <lamanihani1@gmail.com>
2017-12-01 12:08:16 -0600

    Add files via upload

---
+++ email-filter.py
@@ -0,0 +1,253 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Date: Decembre 1, 2017 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%% Weather: It's always cool in the lab %%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%% Health: Overweight %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%% Caffeine: 12975 mg %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%% Hacked: All the things %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 
+# youtube channel : www.youtube.com/c/منضمةالهكرالعربية1
+# github : www.github.com/lamanihani
+# fb : Ha Ni 
+#
+# TNX TO : GOD ^_^ , Mom :v , brahim gone , chelabi el amin, blackroot (khairdine), mohamed karem(wolf-hacker , BROTHERHOOD official :v 
+
+import os
+import sys
+import time
+print('''
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+slowprint("[!] Starting : ")
+time.sleep(5)
+# os.system('clear') for clear the terminal 
+os.system('clear')
+print('''\033[1;31m \033[97m
+        0Mo..........':ldkO0KKXXKK0kxoc,..........kMd
+        0Ml......;d0WMMMMMMMMMMMMMMMMMMMWKx:......kMd
+        0Ml...cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMWO:...kMd         
+        0Ml.lNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNc.kMd               \033[96memail-filtered\033[97m
+        0MdKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM0OMd
+        0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd       By : \033[91mLamani  Hani VEGETA-LFH\033[97m
+        0MxcxWMMMMMNXXNMMMMMMMMMMMMMMMNXXNMMMMMWkcKMd
+        0Md..lMKo,.,'...:kWMMMMMMMNx;...',.;dXMl.'XMd
+        0Mx'.,O;dXMMMXl....:dWMNo;....oXMMMKd;0,.'KMd
+        0MO;.,NMWMMMMMMWk;...XMK...:OWMMMMMMWMN,.cNMd
+        0MxxNMX;KMMKdcclkWN0WMMMN0WNxc:lxXMMk;WMXdKMd
+        0MMMMMO;MMl.......KMXOMNkMk.......xMM.NMMMMMd       F.SOCIETY
+        0MMMMMMXKoclddl;.oWMdkMN,MN:.:ldolcdXNMMMMMMd   
+        0MMMMMMWXMMMMMMMW0KdoNMMdox0MMMMMMMMXMMMMMMMd
+        0MMMMXc'WMMMMMMMMkcWMMMMMMkcMMMMMMMMN'lXMMMMd
+        0MMMd..cMMMMMMMMNdoKMMMMM0x:XMMMMMMMM:..kMMMd
+        0MM0....d0KKOd:.....c0Kx'.....:d0NX0l....NMMd
+        0MMO.....................................WMMd
+        0Mdkc...................................0kOMd
+        0Ml.:Ol;........';;.......;,........':oX:.kMd
+        0Ml..,WMMMMWWWo...';;:c::;'...:WWMMMMMW;..kMd
+        0Ml...dMMMMMMMMKl...........c0MMMMMMMMd...kMd
+        0Ml...cMMMMMMMMMMMXOxdddk0NMMMMMMMMMMM'...kMd
+        0Ml....KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMO....kMd
+        0Ml.....OMMMMMMMMMMMMMMMMMMMMMMMMMMMK.....kMd
+        0Ml......:XMMMMMMMMMMMMMMMMMMMMMMMNl......kMd
+        0Ml........lXMMMMMMMMMMMMMMMMMMMKc........kMd
+        0Ml..........:KMMMMMMMMMMMMMMM0,..........kMd
+        
+   good script !? there is more ^^ 
+   go to : \033[93mwww.github.com/lamanihani
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+#email-type-list : 
+#you can add another type of email ^_^ !
+gmail = ("gmail.com\n")
+yahoo = ('yahoo.com\n')
+yahooo = ('yahoo.fr\n')
+outlook = ('outlook.com\n')
+web = ("web.com\n")
+hotmail = ('hotmail.com\n')
+wanadoo = ("wanadoo.com\n")
+gmx = ("gmx.com\n")
+aol = ("aol.com\n")
+msn = ("msn.com\n")
+live = ("live.com\n")
+yandex = ("yandex.ru\n")
+bk = ("bk.ru\n")
+inbox = ("inbox.ru\n")
+mailru = ("mail.ru\n")
+a = ('prodigy.net\n')
+b = ('rogers.com\n')
+c = ('cox.net\n')
+d = ('centurytel.net\n')
+f = ('juno.com\n')
+g = ('comcast.net\n')
+h = ('sympatico.ca\n')
+l = ('earthlink.net\n')
+k = ('bellsouth.net\n')
+v = ('verizon.net\n')
+# here script ask you to put your email list !
+hani=input("\033[94m[?]\033[97m drag the \033[91memail-list \033[97m:")
+list = open((hani), "r")
+passwords = list.readlines()
+# with slowprint script is COOL :V
+slowprint("\033[94m[!]\033[92m Filtering\033[97m:")
+# here the real work :3 
+#script will read the email-list and he will filtering the email (gmail,outlook,hotmail...)
+#script will open new text and he will write all the email 
+# ex : script will write all the gmail email in text file : gmail.txt
+for lylia in passwords : 
+          if lylia.endswith(gmail) :
+                    file = open("gmail.txt","a")
+                    file.write((lylia)+"")
+                    file.close()        
+          if lylia.endswith(yahoo) :
+                    file = open("yahoo.txt","a")
+                    file.write((lylia)+"")
+                    file.close()    
+          if lylia.endswith(yahooo) :
+                    file = open("yahoo.txt","a")
+                    file.write((lylia)+"")
+                    file.close()          
+          if lylia.endswith(outlook) :
+                    file = open("outlook.txt","a")
+                    file.write((lylia)+"")
+                    file.close()       
+          if lylia.endswith(hotmail) :
+                    file = open("hotmail.txt","a")
+                    file.write((lylia)+"")
+                    file.close()       
+          if lylia.endswith(web) :
+                    file = open("web.txt","a")
+                    file.write((lylia)+"")
+                    file.close()  
+          if lylia.endswith(aol) :
+                    file = open("aol.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(msn) :
+                    file = open("msn.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(a) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(b) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(c) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(d) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(f) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(g) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(h) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(l) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(k) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(v) :
+                    file = open("other.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(live) : 
+                    file = open("live.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(yandex) :
+                    file = open("yandex.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(mailru) : 
+                    file = open("mail.ru.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(bk) : 
+                    file = open("mail.ru.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          if lylia.endswith(inbox) : 
+                    file = open("mail.ru.txt","a")
+                    file.write((lylia)+"")
+                    file.close() 
+          
+# lylia : i love this girl <3 but she dont love me :(
+print("\033[92mDone\033[97m , nice\033[91m phishing\033[97m !")
+# you can add new email type 
+# yourname = ('your.email.type+\n')
+#and add this 
+#if lylia.endswith(yourname) :
+#        file = open("youremailname.txt","a")
+#        file.write((lylia)+"\n")
+#        file.close()
+# ENJOY ;) 
+          
